b27a63183140f0005bca199483e09b9d74db23a8
Miguel Amato <miamato@ucm.es>
2024-02-09 21:01:46 +0100

    Cambiadas las clases lexicas y puesto el main para el juez

--- Tiny0/src/model/lexico/ClaseLexica.java
+++ Tiny0/src/model/lexico/ClaseLexica.java
@@ -1,7 +1,52 @@
 package model.lexico;
 
 public enum ClaseLexica {
-	MAS, MENOS, MUL, DIV, MENOR, MAYOR, MENOREQ, MAYOREQ, IGUAL, DIST, ASIG, 
-	PARAP, PARCIE, PUNYCOMA, COMA, LLAVAP, LLAVCIE, CAMBIOSEC, EVAL, ID, INT, REAL, BOOL,
-	TRUE, FALSE, AND, OR, NOT, EOF, LITENT, LITREAL, PUNTO, ERROR
+    MAS("+"),
+    MENOS("-"),
+    MUL("*"),
+    DIV("/"),
+    MENOR("<"),
+    MAYOR(">"),
+    MENOREQ("<="),
+    MAYOREQ(">="),
+    IGUAL("=="),
+    DIST("!="),
+    ASIG("="),
+    PARAP("("),
+    PARCIE(")"),
+    PUNYCOMA(";"),
+    COMA(","),
+    LLAVAP("{"),
+    LLAVCIE("}"),
+    CAMBIOSEC("&&"),
+    EVAL("@"),
+    ID,
+    INT("<int>"),
+    REAL("<real>"),
+    BOOL("<bool>"),
+    TRUE("<true>"),
+    FALSE("<false>"),
+    AND("<and>"),
+    OR("<or>"),
+    NOT("<not>"),
+    EOF,
+    LITENT,
+    LITREAL,
+    PUNTO("."),
+    ERROR;
+
+    private String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    private ClaseLexica() {
+        image = toString();
+    }
+
+    private ClaseLexica(String image) {
+        this.image = image;
+    }
 }
+

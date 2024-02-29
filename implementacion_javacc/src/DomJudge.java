66bc5656a00bc94066e8cb7c4d19f420803d0f23
Miguel Amato <miamato@ucm.es>
2024-02-29 22:15:33 +0100

    Hecha la implementacion en javacc

---
+++ implementacion_javacc/src/DomJudge.java
@@ -0,0 +1,24 @@
+import asint.AnalizadorSintacticoTiny;
+import asint.AnalizadorSintacticoTinyDJ;
+import asint.ParseException;
+import asint.TokenMgrError;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+public class DomJudge{
+   public static void main(String[] args) throws Exception {
+     try{  
+      AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTinyDJ(new InputStreamReader(System.in));
+      //AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTinyDJ(new InputStreamReader(new FileInputStream("src/imput.txt")));
+      asint.analiza();
+     }
+     catch(ParseException e) {
+        System.out.println("ERROR_SINTACTICO"); 
+     }
+     catch(TokenMgrError e) {
+        System.out.println("ERROR_LEXICO"); 
+     }
+   }
+}
---
+++ implementacion_javacc/src/asint/AnalizadorSintacticoTinyDJ.java
@@ -0,0 +1,43 @@
+package asint;
+
+import java.io.Reader;
+
+
+public class AnalizadorSintacticoTinyDJ extends AnalizadorSintacticoTiny {
+    private void imprime(Token t) {
+        switch(t.kind) {
+            case and: System.out.println("<and>"); break;
+            case or: System.out.println("<or>"); break;
+            case not: System.out.println("<not>"); break;
+            case ent: System.out.println("<int>"); break;
+            case real: System.out.println("<real>"); break;
+            case bool: System.out.println("<bool>"); break;
+            case string: System.out.println("<string>"); break;
+            case nulo: System.out.println("<null>"); break;
+            case falso: System.out.println("<false>"); break;
+            case verdadero: System.out.println("<true>"); break;
+            case proc: System.out.println("<proc>"); break;
+            case If: System.out.println("<if>"); break;
+            case Else: System.out.println("<else>"); break;
+            case While: System.out.println("<while>"); break;
+            case struct: System.out.println("<struct>"); break;
+            case New: System.out.println("<new>"); break;
+            case Delete: System.out.println("<delete>"); break;
+            case read: System.out.println("<read>"); break;
+            case write: System.out.println("<write>"); break;
+            case nl: System.out.println("<nl>"); break;
+            case type: System.out.println("<type>"); break;
+            case call: System.out.println("<call>"); break;
+            case EOF: System.out.println("<EOF>"); break;
+            default: System.out.println(t.image);
+        }
+    }
+    
+    public AnalizadorSintacticoTinyDJ(Reader r) {
+        super(r);
+        disable_tracing();
+    }
+    final protected void trace_token(Token t, String where) {
+        imprime(t);
+    }
+}

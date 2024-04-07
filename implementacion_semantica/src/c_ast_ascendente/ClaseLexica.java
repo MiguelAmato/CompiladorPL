0d10b85c5be9cbb890a092ae14f912ecd5a64a7c
Miguel Amato <miamato@ucm.es>
2024-04-08 00:29:37 +0200

    Hecho el javacup creo

--- implementacion_semantica/src/c_ast_ascendente/ClaseLexica.java
+++ implementacion_semantica/src/c_ast_ascendente/ClaseLexica.java
@@ -8,59 +8,59 @@ package c_ast_ascendente;
 /** CUP generated class containing symbol constants. */
 public class ClaseLexica {
   /* terminals */
-  public static final int DISTINTO = 48;
-  public static final int LITERAL_ENTERO = 50;
-  public static final int PARENTESIS_CIERRE = 30;
-  public static final int PUNTO_Y_COMA = 33;
-  public static final int LITERAL_REAL = 51;
-  public static final int MAYOR_O_IGUAL = 46;
-  public static final int RESTA = 25;
+  public static final int DISTINTO = 51;
+  public static final int LITERAL_ENTERO = 30;
+  public static final int PARENTESIS_CIERRE = 20;
+  public static final int PUNTO_Y_COMA = 22;
+  public static final int LITERAL_REAL = 31;
+  public static final int MAYOR_O_IGUAL = 49;
+  public static final int RESTA = 39;
   public static final int INT = 2;
-  public static final int REFERENCIA = 40;
-  public static final int DELETE = 18;
-  public static final int WRITE = 20;
-  public static final int CAMBIO_DE_SECUENCIA = 41;
-  public static final int IDENTIFICADOR = 49;
-  public static final int NOT = 8;
-  public static final int AND = 6;
-  public static final int CORCHETE_CIERRE = 35;
-  public static final int TYPE = 22;
-  public static final int IGUALDAD = 47;
-  public static final int EVAL = 42;
-  public static final int OR = 7;
+  public static final int REFERENCIA = 27;
+  public static final int DELETE = 12;
+  public static final int WRITE = 14;
+  public static final int CAMBIO_DE_SECUENCIA = 28;
+  public static final int NOT = 45;
+  public static final int AND = 43;
+  public static final int IDENTIFICADOR = 29;
+  public static final int CORCHETE_CIERRE = 23;
+  public static final int TYPE = 16;
+  public static final int IGUALDAD = 50;
+  public static final int OR = 44;
+  public static final int EVAL = 19;
   public static final int BOOL = 4;
-  public static final int CALL = 23;
-  public static final int SUMA = 24;
-  public static final int IF = 13;
-  public static final int LITERAL_CADENA = 52;
+  public static final int CALL = 17;
+  public static final int SUMA = 38;
+  public static final int IF = 7;
+  public static final int LITERAL_CADENA = 32;
   public static final int EOF = 0;
-  public static final int ASIG = 31;
-  public static final int CORCHETE_APERTURA = 34;
-  public static final int TRUE = 10;
-  public static final int MENOR_O_IGUAL = 45;
-  public static final int NEW = 17;
+  public static final int ASIG = 52;
+  public static final int CORCHETE_APERTURA = 36;
+  public static final int TRUE = 34;
+  public static final int MENOR_O_IGUAL = 48;
+  public static final int NEW = 11;
   public static final int error = 1;
-  public static final int COMA = 32;
-  public static final int MODULO = 28;
-  public static final int MENOR = 43;
-  public static final int NULL = 9;
+  public static final int COMA = 21;
+  public static final int MODULO = 42;
+  public static final int MENOR = 46;
+  public static final int NULL = 33;
   public static final int PUNTERO = 37;
-  public static final int MAYOR = 44;
+  public static final int MAYOR = 47;
   public static final int REAL = 3;
-  public static final int ELSE = 14;
-  public static final int PUNTO = 36;
-  public static final int LLAVE_APERTURA = 38;
-  public static final int NL = 21;
-  public static final int READ = 19;
-  public static final int WHILE = 15;
-  public static final int LLAVE_CIERRE = 39;
-  public static final int PROC = 12;
+  public static final int ELSE = 8;
+  public static final int PUNTO = 24;
+  public static final int NL = 15;
+  public static final int LLAVE_APERTURA = 25;
+  public static final int READ = 13;
+  public static final int WHILE = 9;
+  public static final int LLAVE_CIERRE = 26;
+  public static final int PROC = 6;
   public static final int STRING = 5;
-  public static final int PARENTESIS_APERTURA = 29;
-  public static final int FALSE = 11;
-  public static final int DIVISION = 27;
-  public static final int STRUCT = 16;
-  public static final int MULTIPLICACION = 26;
+  public static final int PARENTESIS_APERTURA = 18;
+  public static final int FALSE = 35;
+  public static final int DIVISION = 41;
+  public static final int STRUCT = 10;
+  public static final int MULTIPLICACION = 40;
   public static final String[] terminalNames = new String[] {
   "EOF",
   "error",
@@ -68,12 +68,6 @@ public class ClaseLexica {
   "REAL",
   "BOOL",
   "STRING",
-  "AND",
-  "OR",
-  "NOT",
-  "NULL",
-  "TRUE",
-  "FALSE",
   "PROC",
   "IF",
   "ELSE",
@@ -86,35 +80,41 @@ public class ClaseLexica {
   "NL",
   "TYPE",
   "CALL",
+  "PARENTESIS_APERTURA",
+  "EVAL",
+  "PARENTESIS_CIERRE",
+  "COMA",
+  "PUNTO_Y_COMA",
+  "CORCHETE_CIERRE",
+  "PUNTO",
+  "LLAVE_APERTURA",
+  "LLAVE_CIERRE",
+  "REFERENCIA",
+  "CAMBIO_DE_SECUENCIA",
+  "IDENTIFICADOR",
+  "LITERAL_ENTERO",
+  "LITERAL_REAL",
+  "LITERAL_CADENA",
+  "NULL",
+  "TRUE",
+  "FALSE",
+  "CORCHETE_APERTURA",
+  "PUNTERO",
   "SUMA",
   "RESTA",
   "MULTIPLICACION",
   "DIVISION",
   "MODULO",
-  "PARENTESIS_APERTURA",
-  "PARENTESIS_CIERRE",
-  "ASIG",
-  "COMA",
-  "PUNTO_Y_COMA",
-  "CORCHETE_APERTURA",
-  "CORCHETE_CIERRE",
-  "PUNTO",
-  "PUNTERO",
-  "LLAVE_APERTURA",
-  "LLAVE_CIERRE",
-  "REFERENCIA",
-  "CAMBIO_DE_SECUENCIA",
-  "EVAL",
+  "AND",
+  "OR",
+  "NOT",
   "MENOR",
   "MAYOR",
   "MENOR_O_IGUAL",
   "MAYOR_O_IGUAL",
   "IGUALDAD",
   "DISTINTO",
-  "IDENTIFICADOR",
-  "LITERAL_ENTERO",
-  "LITERAL_REAL",
-  "LITERAL_CADENA"
+  "ASIG"
   };
 }
 

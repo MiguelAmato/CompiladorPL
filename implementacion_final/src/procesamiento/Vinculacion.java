bd9ff85f1b3890324c122b722eca9d8ceb41067b
Miguel Amato <miamato@ucm.es>
2024-05-05 12:45:30 +0200

    Añadido el vinculo

---
+++ implementacion_final/src/procesamiento/Vinculacion.java
@@ -0,0 +1,17 @@
+package procesamiento;
+
+import java.util.Map;
+
+import asint.ProcesamientoDef;
+import asint.SintaxisAbstractaEval.Dec;
+import c_ast_ascendente.UnidadLexica.StringLocalizado;
+
+public class Vinculacion extends ProcesamientoDef {
+	
+	Map<StringLocalizado, Dec> ts;
+
+	public Vinculacion() {
+		
+	}
+
+}
